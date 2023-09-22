2be029dadffb8521daaf5eb256f9f7affcbd2df7
Camden <camdenaws@gmail.com>
2023-09-21 20:54:37 -0700

    Integrate react router. Really cool stuff.

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -5,8 +5,20 @@ import App from './App.tsx'
 import { StoreProvider } from './StoreProvider.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+
 import "missing.css"
 import "missing.css/prism"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 const queryClient = new QueryClient();
 
@@ -14,7 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <StoreProvider>
-                <App />
+                <RouterProvider router={router} />
             </StoreProvider>
         </QueryClientProvider>
     </React.StrictMode>,
