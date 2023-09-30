d4e2a7fa55e624601b6afff3ff50210789a7d2ff
Camden <camdenaws@gmail.com>
2023-09-29 20:40:00 -0700

    Creation of cards seems to work ok

--- packages/frontend/src/Review.tsx
+++ packages/frontend/src/Review.tsx
@@ -1,44 +1,67 @@
 
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, Outlet, useLoaderData } from "react-router-dom";
 import ReactMarkdown from 'react-markdown';
 import rehypePrism from "@mapbox/rehype-prism";
+import { StoreContext } from "./StoreProvider";
 
 const reviewSQL = `
-SELECT 
-    cards.front, 
-    cards.back, 
-    MAX(reviews.due_at) AS latest_due
-FROM 
-    cards 
-INNER JOIN 
-    reviews 
-ON 
-    cards.id = reviews.card_id 
-WHERE 
-    reviews.due_at < unixepoch() 
-GROUP BY
-    reviews.card_id;
+SELECT c.id, c.front, c.back
+FROM cards c
+JOIN (
+    SELECT r.card_id, MAX(r.due_at) AS max_due_at
+    FROM reviews r
+    GROUP BY r.card_id
+) r_max ON c.id = r_max.card_id
+WHERE r_max.max_due_at < unixepoch();
 `
+
+const addReviewSQL = `
+INSERT INTO reviews
+   (id, card_id, due_at, reviewed_at)
+   VALUES
+   (?,?,unixepoch('now', '1 day'),unixepoch())`;
+
 
 const loader = ({ query }) => async ({ request, params }) => {
     return query(reviewSQL, []);
 }
 
 function Review() {
+    const { query } = useContext(StoreContext);
+
     const cardsToReview = useLoaderData();
     const [cards, setCards] = useState(cardsToReview);
     const count = cards.length;
 
     const card = count > 0 && cards[0];
+
+    const [showBack, setShowBack] = useState(false);
+
+    const onReview = () => {
+        setCards(cards => cards.slice(1));
+        setShowBack(false);
+        query(addReviewSQL, [crypto.randomUUID(), card.id])
+    }
+
     return (
         <div>
             {count}
-            {card &&
-                <div>
+            {card ?
+                <div className="f-col justify-content:space-between"
+                style={{ height: "100%"}}>
+                <div className="box">
                     <ReactMarkdown rehypePlugins={[[rehypePrism, { ignoreMissing: true }]]}>
                         {card.front + (card.back !== "" ? "\n\n---\n\n" + card.back : "")}
-                        </ReactMarkdown>
+                    </ReactMarkdown>
+                    </div>
+                    {showBack ?
+                        <div className="toolbar"><button onClick={onReview}>Again</button></div>
+                        : <button onClick={() => setShowBack(true)}>Show back</button>}
+                </div>
+                :
+                <div className="box ok">
+                    Nothing left to review!
                 </div>}
 
         </div>
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { StoreProvider } from "./StoreProvider.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
 
-import Router from "./router";
+import Router from "./router.tsx";
 import "missing.css";
 import "missing.css/prism";
 
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <StoreProvider>
-                <Router />
-            </StoreProvider>
-        </QueryClientProvider>
+        <StoreProvider>
+            <Router />
+        </StoreProvider>
     </React.StrictMode>,
 );
