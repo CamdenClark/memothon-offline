762c162398e762fbe59b88ad3123984781bb7914
Camden <camdenaws@gmail.com>
2023-11-03 23:36:10 -0700

    Update sqlite-wasm package to have real types!

--- packages/frontend/src/StoreProvider.tsx
+++ packages/frontend/src/StoreProvider.tsx
@@ -3,28 +3,37 @@ import { createContext, useEffect, useState } from 'react';
 import * as Comlink from "comlink";
 import QueryWorker from './worker.ts?worker';
 
+interface StoreContextData {
+  query: (sql: string, bind: (string | number)[]) => Promise<any>;
+  cleanup: () => Promise<any>;
+}
+
 interface WorkerData {
-    db: any;
-    init: () => Promise<any>;
-    query: (sql: string, bind: (string | number)[]) => Promise<any>;
-    select: () => any;
-    cleanup: () => Promise<any>;
+  db: any;
+  init: () => Promise<any>;
+  query: (sql: string, bind: (string | number)[]) => Promise<any>;
+  select: () => any;
+  cleanup: () => Promise<any>;
 }
 
 export const worker = Comlink.wrap<WorkerData>(new QueryWorker());
 
 // Create a react context for the store
-export const StoreContext = createContext(null);
+export const StoreContext = createContext<StoreContextData | null>(null);
 
-export const StoreProvider = ({ children }) => {
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        worker.init().then(() => setLoading(false));
-    }, []);
+export type StoreProviderProps = {
+  children: React.ReactNode;
+};
 
-    return (
-        <StoreContext.Provider value={{ query: worker.query, cleanup: worker.cleanup }}>
-            {!loading && children}
-        </StoreContext.Provider>
-    );
+export const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    worker.init().then(() => setLoading(false));
+  }, []);
+
+  return (
+    <StoreContext.Provider value={{ query: worker.query, cleanup: worker.cleanup }}>
+      {!loading && children}
+    </StoreContext.Provider>
+  );
 };
--- packages/frontend/src/worker.ts
+++ packages/frontend/src/worker.ts
@@ -18,48 +18,48 @@ CREATE TABLE IF NOT EXISTS reviews (
 `;
 
 interface WorkerData {
-    db: any;
-    init: () => Promise<any>;
-    query: (sql: string, bind: (string | number)[]) => Promise<any>;
-    select: () => any;
-    cleanup: () => Promise<any>;
+  db: any;
+  init: () => Promise<any>;
+  query: (sql: string, bind: (string | number)[]) => Promise<any>;
+  select: () => any;
+  cleanup: () => Promise<any>;
 }
 
 const data: WorkerData = {
-    db: null,
-    async init() {
-        const sqlite3 = await sqlite3InitModule({
-            print: console.log,
-            printErr: console.error,
-        });
-        console.log('Done initializing. Running demo...');
-        try {
-            if ('opfs' in sqlite3) {
-                this.db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
-                console.log('OPFS is available, created persisted database at', this.db.filename);
-                this.query(up, []);
-            } else {
-                this.db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
-                console.log('OPFS is not available, created transient database', this.db.filename);
-            }
-        } catch (err: any) {
-            console.error(err.name, err.message);
-        }
-    },
-    query(sql: string, bind: any) {
-        if (!this.db) {
-            return;
-        }
-        return this.db.exec({ sql, bind, returnValue: "resultRows", rowMode: "object" });
-    },
-    select() {
-        return this.query("SELECT * from cards", []);
-    },
-    async cleanup() {
-        const opfsRoot = await navigator.storage.getDirectory();
-        await opfsRoot.removeEntry("mydb.sqlite3");
-        await this.init();
+  db: null,
+  async init() {
+    const sqlite3 = await sqlite3InitModule({
+      print: console.log,
+      printErr: console.error,
+    });
+    console.log('Done initializing. Running demo...');
+    try {
+      if ('opfs' in sqlite3) {
+        this.db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3', 'ct');
+        console.log('OPFS is available, created persisted database at', this.db.filename);
+        this.query(up, []);
+      } else {
+        this.db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
+        console.log('OPFS is not available, created transient database', this.db.filename);
+      }
+    } catch (err: any) {
+      console.error(err.name, err.message);
     }
+  },
+  query(sql: string, bind: any) {
+    if (!this.db) {
+      return;
+    }
+    return this.db.exec({ sql, bind, returnValue: "resultRows", rowMode: "object" });
+  },
+  select() {
+    return this.query("SELECT * from cards", []);
+  },
+  async cleanup() {
+    const opfsRoot = await navigator.storage.getDirectory();
+    await opfsRoot.removeEntry("mydb.sqlite3");
+    await this.init();
+  }
 }
 
 Comlink.expose(data);
